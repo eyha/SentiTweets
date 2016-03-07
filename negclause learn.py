44c64cd9148d768405f99a2da790e7ec48095c27
eyha <psyea2@nottingham.ac.uk>
2016-03-07 12:00:24 +0000

    Function negclause
    Rewrote it as a function that takes sentiments as a variable.

--- negclause learn.py
+++ negclause learn.py
@@ -58,61 +58,68 @@ for row in reader:
     rownum += 1
 csvfile.close()
 
-#Divide into training and test data - randomly allocate 4/5 to training and 1/5 to test
-position = []
-posPosts = []
-negPosts = []
-neuPosts = []
-for posts in range(0,len(sentiments)):
-    position.insert(posts,random.randint(0,4))
-    if position[posts] != 0:
-        posNegs = negCheck(tokens[posts])
-        if sentiments[posts] == 4:
-            posPosts.extend(posNegs[0])
-            negPosts.extend(posNegs[1])
-        elif sentiments[posts] == 0:
-            negPosts.extend(posNegs[1])
-            posPosts.extend(posNegs[0])
-        else:
-            neuPosts.extend(posNegs[0])
-            neuPosts.extend(posNegs[1])
+def negTesting(sentiments):
+    #Divide into training and test data - randomly allocate 4/5 to training and 1/5 to test
+    position = []
+    posPosts = []
+    negPosts = []
+    neuPosts = []
+    for posts in range(0,len(sentiments)):
+        position.insert(posts,random.randint(0,4))
+        if position[posts] != 0:
+            posNegs = negCheck(tokens[posts])
+            if sentiments[posts] == 4:
+                posPosts.extend(posNegs[0])
+                negPosts.extend(posNegs[1])
+            elif sentiments[posts] == 0:
+                negPosts.extend(posNegs[1])
+                posPosts.extend(posNegs[0])
+            else:
+                neuPosts.extend(posNegs[0])
+                neuPosts.extend(posNegs[1])
 
-wordBag = defaultdict(list)
-for token in posPosts:
-    wordBag[token].append(5)
-for token in negPosts:
-    wordBag[token].append(-5)
+    wordBag = defaultdict(list)
+    for token in posPosts:
+        wordBag[token].append(5)
+    for token in negPosts:
+        wordBag[token].append(-5)
 
-for token in neuPosts:
-    wordBag[token].append(0)
+    for token in neuPosts:
+        wordBag[token].append(0)
 
-wordSen = {}
-for word in wordBag:
-##    print("Word: " + word + ", instances: " + str(len(wordBag[word])) + ", values: ")
-##    for value in wordBag[word]:
-##            print(value, end=" ")
-##    print("Total = " + str(sum(wordBag[word])))
-    wordSen[word] = sum(wordBag[word]) / float(len(wordBag[word]))
-##    print("word: " + word + ", word sentiment: " + str(wordSen[word]))
+    wordSen = {}
+    for word in wordBag:
+    ##    print("Word: " + word + ", instances: " + str(len(wordBag[word])) + ", values: ")
+    ##    for value in wordBag[word]:
+    ##            print(value, end=" ")
+    ##    print("Total = " + str(sum(wordBag[word])))
+        wordSen[word] = sum(wordBag[word]) / float(len(wordBag[word]))
+    ##    print("word: " + word + ", word sentiment: " + str(wordSen[word]))
+        
+    #testing phase
+    corrects = []
+    for posts in range(0,len(sentiments)):
+        if position[posts] == 0:
+            score = 0
+            for token in tokens[posts]:
+                if token in wordSen:
+                    score += float(wordSen[token])
+    ##                print(score)
+            if score >= 2.5:
+                corrects.append(sentiments[posts] == 4)
+    ##            print("postive: " + str(sentiments[posts] == 4))
+            elif score <= -2.5:
+                corrects.append(sentiments[posts] == 0)
+    ##            print("negative: " + str(sentiments[posts] == 0))
+            else:
+                corrects.append(sentiments[posts] == 2)
+    ##            print("neutral: " + str(sentiments[posts] == 2))
+                
+    ##print(corrects)
+    numCorrects = sum(corrects)
+    numTestCases = len(corrects)
+    accuracy = float(numCorrects * 100/float(numTestCases))
+    print("The number of correctly predicted posts is " + str(numCorrects) + " out of " + str(numTestCases))
+    return accuracy
     
-#testing phase
-corrects = []
-for posts in range(0,len(sentiments)):
-    if position[posts] == 0:
-        score = 0
-        for token in tokens[posts]:
-            if token in wordSen:
-                score += float(wordSen[token])
-##                print(score)
-        if score >= 2.5:
-            corrects.append(sentiments[posts] == 4)
-##            print("postive: " + str(sentiments[posts] == 4))
-        elif score <= -2.5:
-            corrects.append(sentiments[posts] == 0)
-##            print("negative: " + str(sentiments[posts] == 0))
-        else:
-            corrects.append(sentiments[posts] == 2)
-##            print("neutral: " + str(sentiments[posts] == 2))
-            
-##print(corrects)
-print("The number of correctly predicted posts is " + str(sum(corrects)) + " out of " + str(len(corrects)))
+print ("Total Accuracy: " + str(negTesting(sentiments)) + "%")
