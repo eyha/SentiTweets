6e3f9c38bd2977d40245c8d95b16d85cd00bfe86
eyha <psyea2@nottingham.ac.uk>
2016-03-07 04:42:26 +0000

    Emotiden work
    Fixed emoticon classifier

--- emotIden.py
+++ emotIden.py
@@ -1,10 +1,10 @@
 import re
 
 def findEmotes(wordBag):
-    # match all tokens that have consecutive special characters
+    # match all tokens that have more than one special characters - need to handle HTML special characters
     # - can't handle ones containing whitespace, as tokens are separated by whitespace anyway
     # - ignores examples beginning with #, as these are assumed to be hashtags 
-    nonword = re.compile('[^0-9a-zA-Z#][^0-9a-zA-Z]+')
+    nonword = re.compile('((&(.*);)|[^\s0-9a-zA-Z#])\S*((&(.*);)|[^\s0-9a-zA-Z])+\S*')
     emoticons = []
     for word in wordBag:
         if nonword.match(word):
