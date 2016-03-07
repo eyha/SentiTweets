db51b1ea495452cd8af5324d80bf9aa24341deaf
eyha <psyea2@nottingham.ac.uk>
2016-03-07 10:21:09 +0000

    Tests
    Rewrote the cross validation as a function that saves the results to
    file.

--- SentiTweets.py
+++ SentiTweets.py
@@ -6,6 +6,9 @@ import csv
 import random
 from collections import defaultdict
 
+tests = []
+accuracy = 0
+
 tokenizer = TweetTokenizer()
 csvfile = open('trainingandtestdata/testdata.manual.2009.06.14.csv', 'rb')
 reader = csv.reader(csvfile, delimiter=',')
@@ -28,57 +31,71 @@ for row in reader:
     rownum += 1
 csvfile.close()
 
-#Divide into training and test data - randomly allocate 4/5 to training and 1/5 to test
-position = []
-posPosts = []
-negPosts = []
-neuPosts = []
-for posts in range(0,len(sentiments)):
-    position.insert(posts,random.randint(0,4))
-    if position[posts] != 0:
-        if sentiments[posts] == 4:
-            posPosts.extend(tokens[posts])
-        elif sentiments[posts] == 0:
-            negPosts.extend(tokens[posts])
-        else:
-            neuPosts.extend(tokens[posts])
+def test():
+    #Divide into training and test data - randomly allocate 4/5 to training and 1/5 to test
+    position = []
+    posPosts = []
+    negPosts = []
+    neuPosts = []
+    for posts in range(0,len(sentiments)):
+        position.insert(posts,random.randint(0,4))
+        if position[posts] != 0:
+            if sentiments[posts] == 4:
+                posPosts.extend(tokens[posts])
+            elif sentiments[posts] == 0:
+                negPosts.extend(tokens[posts])
+            else:
+                neuPosts.extend(tokens[posts])
 
-wordBag = defaultdict(list)
-for token in posPosts:
-    wordBag[token].append(5)
-for token in negPosts:
-    wordBag[token].append(-5)
+    wordBag = defaultdict(list)
+    for token in posPosts:
+        wordBag[token].append(5)
+    for token in negPosts:
+        wordBag[token].append(-5)
 
-for token in neuPosts:
-    wordBag[token].append(0)
+    for token in neuPosts:
+        wordBag[token].append(0)
 
-wordSen = {}
-for word in wordBag:
-##    print("Word: " + word + ", instances: " + str(len(wordBag[word])) + ", values: ")
-##    for value in wordBag[word]:
-##            print(value, end=" ")
-##    print("Total = " + str(sum(wordBag[word])))
-    wordSen[word] = sum(wordBag[word]) / float(len(wordBag[word]))
-##    print("word: " + word + ", word sentiment: " + str(wordSen[word]))
+    wordSen = {}
+    for word in wordBag:
+    ##    print("Word: " + word + ", instances: " + str(len(wordBag[word])) + ", values: ")
+    ##    for value in wordBag[word]:
+    ##            print(value, end=" ")
+    ##    print("Total = " + str(sum(wordBag[word])))
+        wordSen[word] = sum(wordBag[word]) / float(len(wordBag[word]))
+    ##    print("word: " + word + ", word sentiment: " + str(wordSen[word]))
+        
+    #testing phase
+    corrects = []
+    for posts in range(0,len(sentiments)):
+        if position[posts] == 0:
+            score = 0
+            for token in tokens[posts]:
+                if token in wordSen:
+                    score += float(wordSen[token])
+    ##                print(score)
+            if score >= 2.5:
+                corrects.append(sentiments[posts] == 4)
+    ##            print("postive: " + str(sentiments[posts] == 4))
+            elif score <= -2.5:
+                corrects.append(sentiments[posts] == 0)
+    ##            print("negative: " + str(sentiments[posts] == 0))
+            else:
+                corrects.append(sentiments[posts] == 2)
+    ##            print("neutral: " + str(sentiments[posts] == 2))
+
+    ##print(corrects)
+    numCorrects = sum(corrects)
+    numTests = len(corrects)
+    accuracy = float(numCorrects*100/float(numTests))
+    tests.append(accuracy)
+    print("The number of correctly predicted posts is " + str(numCorrects) + " out of " + str(numTests) + ".")
+    print("The accuracy was " + str(accuracy) + "%")
+
     
-#testing phase
-corrects = []
-for posts in range(0,len(sentiments)):
-    if position[posts] == 0:
-        score = 0
-        for token in tokens[posts]:
-            if token in wordSen:
-                score += float(wordSen[token])
-##                print(score)
-        if score >= 2.5:
-            corrects.append(sentiments[posts] == 4)
-##            print("postive: " + str(sentiments[posts] == 4))
-        elif score <= -2.5:
-            corrects.append(sentiments[posts] == 0)
-##            print("negative: " + str(sentiments[posts] == 0))
-        else:
-            corrects.append(sentiments[posts] == 2)
-##            print("neutral: " + str(sentiments[posts] == 2))
-            
-##print(corrects)
-print("The number of correctly predicted posts is " + str(sum(corrects)) + " out of " + str(len(corrects)))
+for t in range(0,5):
+    test()
+output = open('SentiTweetsOutput.csv', 'ab')
+writer = csv.writer(output, 'excel')
+writer.writerow(tests)
+output.close()
